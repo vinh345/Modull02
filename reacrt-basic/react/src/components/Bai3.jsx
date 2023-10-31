7b01c13f14535a083717361751a5c26a56d2d5da
vinh345 <141289079+vinh345@users.noreply.github.com>
2023-10-31 22:05:14 +0700

    Delete reacrt-basic/react directory

--- reacrt-basic/react/src/components/Bai3.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class  extends Component {
-  render() {
-    return (
-      <div>Bây giờ là 14:19:25</div>
-    )
-  }
-}
--- reacrt-basic/react/src/components/Bai6Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Bai6Navbar extends Component {
-  render() {
-    return (
-      <div>Bai6Navbar</div>
-    )
-  }
-}
